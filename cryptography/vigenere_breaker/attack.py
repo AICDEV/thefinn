acb931d8f3d4f4a6f5129766b4fa52b7e9cea0e3
aicdev <doc.aicdev@gmail.com>
2021-01-07 15:02:14 +0100

    udate attack script

--- cryptography/vigenere_breaker/attack.py
+++ cryptography/vigenere_breaker/attack.py
@@ -2,8 +2,8 @@ import sys
 import argparse
 import string
 import re
-from itertools import chain
 import math
+import frequency
 
 UPPER_DICT = list(string.ascii_uppercase)
 
@@ -73,12 +73,6 @@ def factorise(n):
             fac.add(i)
             if n // i != i:
                 fac.add(n // i)
-    # for i in chain([2], range(3, n)):
-    #     while n % i == 0:
-    #         fac.add(i)
-    #         n = n // i
-    #     if i > n:
-    #         break
     return fac
 
 
@@ -102,10 +96,53 @@ GET KEY MAP
 def get_sorted_key_map(frequent_numbers):
     return sorted(frequent_numbers.items(), key=lambda item: item[1], reverse=True)
 
+"""
+GET MESSAGE PARTS
+"""
+def get_message_parts(message, length):
+    parts = []
+    for a in range(0, length):
+        counter = a
+        part = ""
+        while counter < len(message):
+            part += message[counter]
+            counter += length
+        parts.append(part)
+    return parts
+
+"""
+CLEAN MESSAGE
+"""
 def parse_message(message):
     message = message.replace("\n", "")
     message = message.upper()
     return re.sub(r'\W', '', message)
+
+"""
+BRUTEFORCE
+"""
+def rotate(text, key):
+    try:
+        rotated = ""
+        for c in text:
+            origin = UPPER_DICT.index(c.upper())
+            target = origin - key
+
+            rotated += UPPER_DICT[(target % len(UPPER_DICT))]
+        return rotated
+    except:
+        return text
+
+def bruteforce(parts):
+    print("bruteforcing")
+    for part in parts:
+        key = part[0]
+        print("rotate for key:{}".format(key))
+        for text in part[1]:
+            print("*"*40)
+            for a in range(1,27):
+                score = frequency.get_score(frequency.get_letter_map(rotate(text,a)))
+                print(score)
 
 
 def main():
@@ -120,34 +157,38 @@ def main():
         print("-"*40)
         print("analyse text for triagrams etc")
         multiples = search_occurences(parsed_message)
-        print(multiples)
+
 
         print("extracting positions")
         positions = []
         for m in multiples:
             for word in m[1]:
                 positions.append(search_positions(parsed_message, word))
-        #print(positions)
 
-        #print("calculating distances")
         distances = calculate_distances(positions)
         fact_arr = []
         for dis in distances:
             fact_arr.append(factorise(dis))
 
-        #print(fact_arr)
 
         frequent_numbers = get_most_frequent_number(fact_arr)
-        #print(frequent_numbers)
         key_map = get_sorted_key_map(frequent_numbers)
-        #get_key_map(frequent_numbers)
+
 
         print("found the following possible key lengths")
+        part_arr = []
         c = 1
         for key in key_map:
-            print("rank: {} -> key with length: {}".format(c, key[0]))
+            # print("rank: {} -> key with length: {}".format(c, key[0]))
+            # print("extract message parts")
+            # part_arr.append((key[0],get_message_parts(parsed_message, key[0])))
             c += 1
+        
+        print("rank: {} -> key with length: {}".format(4, 4))
+        print("extract message parts")
+        part_arr.append((4,get_message_parts(parsed_message, 4)))
 
+        bruteforce(part_arr)
     except Exception as e:
         print(e)
         sys.exit(1)
